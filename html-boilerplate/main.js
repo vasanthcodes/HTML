e68eef7896432033bc0e9ff63907caf6f884f1e5
vasanthcodes <vasanthsanku2719@gmail.com>
2025-01-19 11:39:28 +0530

    practised few exercises

--- html-boilerplate/main.js
+++ html-boilerplate/main.js
@@ -12,8 +12,27 @@ div.classList.add("smthg")
 div.textContent="Hey! I'm red"
 container.appendChild(div)
 
+
+
 const h3=document.createElement("h3")
 h3.style.color="blue"
 h3.classList.add("h3ig")
 h3.textContent="I'm a blue h3"
 container.appendChild(h3)
+
+const mainh=document.querySelector(".somthg")
+
+const div2=document.createElement("div")
+div2.setAttribute("class","div")
+div2.setAttribute("style","background-color:pink;border-color:pink;border-width:20px;")
+mainh.appendChild(div2)
+
+const hh1=document.createElement("h1")
+hh1.textContent="Im in a div"
+div2.appendChild(hh1)
+
+let p=document.createElement("p")
+p.textContent="ME TOO!"
+div2.appendChild(p)
+
+
